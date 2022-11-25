b38b1fb0f09fd73fc842ea94a6b1d2d6b717656a
a pomaska <a.pomaska.ap@gmail.com>
2022-11-25 22:50:14 +0100

    working version

--- Exercise.py
+++ Exercise.py
@@ -2,8 +2,14 @@
 import CheckIt
 
 def trzy(a, b, c):
-    # zdefiniuj funkcję i sprawdź jej działanie
-    pass
+    if a > b:
+        pom = a
+    else:
+        pom = b
+    if pom > c:
+        return pom
+    else:
+        return c
 
 print("")
 print("Wyniki testów:")
@@ -12,7 +18,7 @@ print(" Test                  | Oczekiwano | Otrzymano | Wynik")
 print("-----------------------+------------+-----------+-----------")
 print(" trzy(10, 10, 10)      | 10         | ", trzy(10, 10, 10),"      |", CheckIt.checkIt(10, trzy(10, 10, 10)))
 print("-----------------------+------------+-----------+-----------")
-print(" trzy(112, 99, 112)    | 112        | ", trzy(112, 99, 112),"       |", CheckIt.checkIt(112, trzy(112, 99, 112)))
+print(" trzy(112, 99, 112)    | 112        | ", trzy(112, 99, 112),"     |", CheckIt.checkIt(112, trzy(112, 99, 112)))
 print("-----------------------+------------+-----------+-----------")
-print(" trzy(456, 456, 455)   | 456        | ", trzy(456, 456, 455),"       |", CheckIt.checkIt(456, trzy(456, 456, 455)))
+print(" trzy(456, 456, 455)   | 456        | ", trzy(456, 456, 455),"     |", CheckIt.checkIt(456, trzy(456, 456, 455)))
 print("")
