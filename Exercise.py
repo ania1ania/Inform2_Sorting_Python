8a218800b08032901ad94d6c50e3095261996c4c
a pomaska <a.pomaska.ap@gmail.com>
2022-11-26 17:08:35 +0100

    working version

--- Exercise.py
+++ Exercise.py
@@ -1,8 +1,18 @@
 import CheckIt
 
 def w(t, x):
-    # zdefiniuj funkcję i sprawdź jej działanie
-    pass
+    v = []
+    inserted = 0    # flaga informacyjna, że liczba x byla już wstawiona bo jest jeden warunek
+                    # można to zrobić inaczej - przepisując z t do v sprawdzać czy
+                    # t[i] < x < t[i+1]
+    for i in range(len(t)):
+        if x < t[i] and 0 == inserted:
+            v.append(x)
+            inserted = 1
+        v.append(t[i])
+        if i == len(t)-1 and x > t[i]:    # przypadek gdy trzeba wstawić na końcu listy
+            v.append(x)
+    return v
 
 print("")
 print("Wyniki testów:")
